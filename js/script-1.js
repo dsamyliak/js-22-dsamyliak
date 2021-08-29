a8c7de48fa3417d0b458d30eaf177f3228c8faac
Dmytro Samyliak <dsamyliak@gmail.com>
2021-08-29 13:34:22 +0300

    Update script-1.js
    Update code

--- js/script-1.js
+++ js/script-1.js
@@ -1,13 +1,13 @@
 
 const priceTop = 100;
 const description = "About product";
-let xyz;
-
-// xyz = prompt('Text', xyz);
+let xyz = 'Text';
 
 console.log("Top price =", priceTop);
 console.log(description);
-console.log("xyz = ", xyz);
+console.log("xyz =",xyz);
+
+
 
 // const shouldRenew = confirm("Do you want to renew subscribing?");
 // console.log(shouldRenew);
@@ -16,3 +16,6 @@ console.log("xyz = ", xyz);
 // quantity = Number(quantity);
 // console.log(quantity);
 // console.log(typeof quantity);
+
+// ------------------------------------------------------------------------
+// 03-numbers
