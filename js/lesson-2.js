17e563c515ec6a3c9a8261dc31ed90c64a8efdfc
Dmytro Samyliak <dsamyliak@gmail.com>
2021-09-06 11:35:37 +0300

    add lesson-2

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -1,3 +1,12 @@
 // ------------------------------------------------------------------------
-// 
+// HW - Module 2 - Task 10
+
+function splitMessage(message, delimeter) {
+  let words;
+  // Change code below this line
+words = message.split(delimeter);
+  // Change code above this line
+  return words;
+}
+console.log(splitMessage("Mango asdsad sadasd text cool", " "));
 
