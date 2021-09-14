3a528af955254bdd691fa43685f6252d36ec34c7
Dmytro Samyliak <dsamyliak@gmail.com>
2021-09-14 08:51:45 +0300

    Add lesson-3

---
+++ js/lesson-3.js
@@ -0,0 +1,6 @@
+// Online webinar 3 Kyryl Melnyk
+//Репозиторий https://github.com/VovaMelnyk/js-group-39
+//mdn - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
+
+
+  
