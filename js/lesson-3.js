5c12d634633f71d305fb1687f60cd83e103d140a
Dmytro Samyliak <dsamyliak@gmail.com>
2021-09-14 09:01:58 +0300

    Change main branch and add hw-3

--- js/lesson-3.js
+++
@@ -1,6 +0,0 @@
-// Online webinar 3 Kyryl Melnyk
-//Репозиторий https://github.com/VovaMelnyk/js-group-39
-//mdn - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
-
-
-  
