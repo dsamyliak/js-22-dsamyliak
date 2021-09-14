4ebc89403d9f4647bafe6f6298945da7eef34aec
Dmytro Samyliak <dsamyliak@gmail.com>
2021-09-14 14:56:11 +0300

    Add new tasks with code

--- js/hw-module-3.js
+++ js/hw-module-3.js
@@ -78,7 +78,33 @@
 // ------------------------------------------------------------------------
 // HW-3 - Task 4 - Доступ к вложенным свойствам
 
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
 
+// // Change code below this line
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[apartment.tags.length - 1];
+// // Change code above this line
+// console.log(ownerName);
+// console.log(ownerPhone);
+// console.log(ownerEmail);
+// console.log(numberOfTags);
+// console.log(firstTag);
+// console.log(lastTag);
 
 // ------------------------------------------------------------------------
 // ------------------------------------------------------------------------
@@ -129,3 +155,148 @@
 // console.log(averageRating); // 8.2
 // ------------------------------------------------------------------------
 // ------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------
+// HW-3 - Task 5 - Доступ к свойствам через квадратные скобки
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// // Change code below this line
+// const aptRating = apartment["rating"];
+// const aptDescr = apartment["descr"];
+// const aptPrice = apartment["price"];
+// const aptTags = apartment["tags"];
+// // Change code above this line
+
+// console.log(aptRating);
+// console.log(aptDescr);
+// console.log(aptPrice);
+// console.log(aptTags);
+
+// ------------------------------------------------------------------------
+// HW-3 - Task 6 - Изменение значения свойства
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = "Henry Sibola";
+// apartment.tags.push("trusted");
+
+// console.log(apartment);
+
+// ------------------------------------------------------------------------
+// HW-3 - Task 7 - Добавление свойств
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = { country: "Jamaica", city: "Kingston", };
+
+
+// console.log(apartment);
+// console.log(apartment.location);
+// console.log(apartment.location.country);
+// console.log(apartment.location.city);
+
+// ------------------------------------------------------------------------
+// HW-3 - Task 8 - Короткие свойства
+
+// const name = "Repair Droid";
+// const price = 2500;
+// const image = "https://via.placeholder.com/640x480";
+// const tags = ["on sale", "trending", "best buy"];
+
+// const product = {
+//   // Change code below this line
+//   name,
+//   price,
+//   image,
+//   tags,
+//   // Change code above this line
+// };
+
+// console.log(product);
+
+// ------------------------------------------------------------------------
+// HW-3 - Task 9 - Вычисляемые свойства
+
+// const propName = "name";
+// const propSex = "sex";
+// const user = {
+//   age: 25
+// };
+
+// user[propName] = "Генри Сибола";
+// user[propSex] = "Man";
+
+
+// console.log(user.name); // "Генри Сибола"
+// console.log(user.age); // 25
+// console.log(user); // age: 25, name: "Генри Сибола", sex: "Man",
+
+// ------------------------------------------------------------------------
+
+// const propName = "name";
+// const propSex = "sex";
+// const user = {
+//     age: 25,
+//     [propName]: "Генри Сибола",
+//     [propSex]: "Man",
+// };
+
+
+// console.log(user.name); // "Генри Сибола"
+// console.log(user.age); // 25
+// console.log(user.sex); // "Man"
+// console.log(user); // age: 25, name: "Генри Сибола", sex: "Man",
+
+// ------------------------------------------------------------------------
+
+// const emailInputName = "email";
+// const passwordInputName = "password";
+
+// const credentials = {
+//   // Change code below this line
+//     [emailInputName]: "henry.carter@aptmail.com",
+//     [passwordInputName]: "jqueryismyjam",
+//   // Change code above this line
+// };
+
+// console.log(credentials.email);
+// console.log(credentials.password);
+
+// ------------------------------------------------------------------------
+// HW-3 - Task 10 - Цикл for...in
+
