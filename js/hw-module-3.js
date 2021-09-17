1365789862e5f1e7377289155e72e52ca772a9ba
Dmytro Samyliak <dsamyliak@gmail.com>
2021-09-17 18:15:42 +0300

    Add finished Task 24

--- js/hw-module-3.js
+++ js/hw-module-3.js
@@ -642,20 +642,31 @@
 // ------------------------------------------------------------------------
 // HW-3 - Task 24 - Деструктуризация в циклах
 
-const colors = [
-  { hex: "#f44336", rgb: "244,67,54" },
-  { hex: "#2196f3", rgb: "33,150,243" },
-  { hex: "#4caf50", rgb: "76,175,80" },
-  { hex: "#ffeb3b", rgb: "255,235,59" },
-];
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
 
-const hexColors = [];
-const rgbColors = [];
-// Change code below this line
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
 
-for (const color of colors) {
-  hexColors.push(color.hex);
-  rgbColors.push(color.rgb);
-}
+// // for (const color of colors) {
+// //   hexColors.push(color.hex);
+// //   rgbColors.push(color.rgb);
+// // }
 
+// for (const {hex , rgb,} of colors) {
+     
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+// ------------------------------------------------------------------------
+// HW-3 - Task 25 - Глубокая деструктуризация
 
