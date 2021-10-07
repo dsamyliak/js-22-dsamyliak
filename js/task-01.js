e21c107c29cc6a192457c5c2612715c325310ab3
Dmytro Samyliak <dsamyliak@gmail.com>
2021-10-07 16:20:05 +0300

    Add tasks from hw-6 to hw-10

---
+++ js/task-01.js
@@ -0,0 +1,27 @@
+
+//random task
+//---------------------
+// let obj = {
+//  "0": 1,
+//  0: 3,
+// };
+
+// console.log(obj);
+// console.log(obj["0"]);
+// console.log(obj[0]);
+// alert( obj["0"] + obj[0] );
+//---------------------
+// 01-query-selectors
+// const magicBtn = document.querySelector('.js-magic-btn');
+// // console.log('magicBtn:', magicBtn);
+
+// magicBtn.addEventListener('click', () => {
+// const navEl = document.querySelector('.item');
+// console.log('navEl:', navEl);
+
+// const navEls = document.querySelectorAll('.item');
+// console.log('navEls:', navEls);
+
+// const ulByIdCategories = document.querySelector('#categories');
+// console.log('ulByIdCategories:', ulByIdCategories);
+// });
