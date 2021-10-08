387f154c9f843dfd54b74cc8bb38eae10c8dea8e
Dmytro Samyliak <dsamyliak@gmail.com>
2021-10-08 19:31:45 +0300

    Update with 03-classlist

--- js/task-01.js
+++ js/task-01.js
@@ -11,6 +11,10 @@
 // console.log(obj[0]);
 // alert( obj["0"] + obj[0] );
 
+document.querySelector('.img-js-this').childNodes[2].style['top'] = '300px';
+document.querySelector('.img-js-this').childNodes[2].style['left'] = '300px';
+
+
 //---------------------
 // 01-query-selectors
 //---------------------
@@ -32,21 +36,19 @@
 //---------------------
 // 02-properties
 //---------------------
-document.querySelector('.img-js-this').childNodes[2].style['top'] = '300px';
-document.querySelector('.img-js-this').childNodes[2].style['left'] = '300px';
 
-const magicBtn = document.querySelector('.js-magic-btn');
+// const magicBtn = document.querySelector('.js-magic-btn');
 
-magicBtn.addEventListener('click', () => {
-    const imgEl = document.querySelector('.img-js-this');
-    console.log('imgEl', imgEl);
-    console.log(imgEl.querySelector('a'));
+// magicBtn.addEventListener('click', () => {
+//     const imgEl = document.querySelector('.img-js-this');
+//     console.log('imgEl', imgEl);
+//     console.log(imgEl.querySelector('a'));
     
-    imgEl.src = "./images/this1.png";
-    imgEl.alt = 'JS this';
-    console.log('', imgEl.src);
-    console.log('imgEl', imgEl);
-});
+//     imgEl.src = "./images/this1.png";
+//     imgEl.alt = 'JS this';
+//     console.log('', imgEl.src);
+//     console.log('imgEl', imgEl);
+// });
 
 // const task01Title = document.querySelectorAll('h2');
 // console.log('task01Title', task01Title);
@@ -79,11 +81,37 @@ magicBtn.addEventListener('click', () => {
 // console.log(aThisObj.style['top'], aThisObj.style['left']);
 // console.log(aThisObj.getAttribute('style'));
 
-const actions = document.querySelectorAll('.js-actions button');
-console.log(actions);
-console.log(actions[0].dataset);
-console.log(actions[0].dataset.action);
-console.log(actions[1].dataset.action);
-console.log(actions[2].dataset);
-console.log('action =', actions[2].dataset.action, '; color =', actions[2].dataset.color, ';');
+// const actions = document.querySelectorAll('.js-actions button');
+// console.log(actions);
+// console.log(actions[0].dataset);
+// console.log(actions[0].dataset.action);
+// console.log(actions[1].dataset.action);
+// console.log(actions[2].dataset);
+// console.log('action =', actions[2].dataset.action, '; color =', actions[2].dataset.color, ';');
 
+// magicBtn.addEventListener('click', () => {
+//     const inputEl = document.querySelector('.js-input-text');
+//     console.log(inputEl);
+//     console.log(inputEl.value);
+//     inputEl.value = '';
+// });
+
+//---------------------
+// 03-classlist
+//---------------------
+
+const magicBtn = document.querySelector('.js-magic-btn');
+
+magicBtn.addEventListener('click', () => {
+    const navEl = document.querySelector('.title-categorie');
+    console.log(navEl.classList);
+    navEl.classList.add('animals-class', 'first-class');   
+});
+
+const removeBtn = document.querySelector('.remove-class');
+
+removeBtn.addEventListener('click', () => {
+    const navEl = document.querySelector('.title-categorie');
+    console.log(navEl.classList);
+    navEl.classList.remove('first-class');
+});
