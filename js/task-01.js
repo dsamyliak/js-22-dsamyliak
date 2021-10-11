debb45758af32024f1ee8afaa516cd02a4effa58
Dmytro Samyliak <dsamyliak@gmail.com>
2021-10-11 21:00:44 +0300

    Update task-01 with 04-dom-traversal

--- js/task-01.js
+++ js/task-01.js
@@ -117,23 +117,6 @@ document.querySelector('.img-js-this').childNodes[2].style['left'] = '300px';
 // });
 
 //---------------------
-// HW-6 Task 1
-//---------------------
-// const categoriesList = document.querySelector('ul#categories');
-// console.log('Number of categories:', categoriesList.children.length);
-// console.log('');
-// // console.log(categoriesList);
-// // console.log(categoriesList.innerHTML);
-// // console.log(categoriesList.children[0].querySelectorAll('li').length);
-
-// const h2arrayCategories = document.querySelectorAll('h2');
-// for (let index = 0; index < h2arrayCategories.length; index++) {
-//     console.log(`Category: ${h2arrayCategories[index].textContent}`);
-//     console.log(`Elements: ${categoriesList.children[index].querySelectorAll('li').length}`, );
-//     console.log("");
-// };
-
-//---------------------
 // 04-dom-traversal
 //---------------------
 
