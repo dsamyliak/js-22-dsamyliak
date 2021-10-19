59445a6b49c3be8eea5a041c7263d114f0b884f3
Dmytro Samyliak <dsamyliak@gmail.com>
2021-10-19 13:12:35 +0300

    Add part cw-7

--- js/hw-module-7.js
+++ js/hw-module-7.js
@@ -41,11 +41,38 @@
 
 
 // !!!!!!!!!!!!!!!!!!!!!!!!
+// Using lodash library
+console.log('Repeating executed by 2 scripts adding in html!!!');
 
-console.log(_.divide(7,2));
+console.log("_.divide(7,2) = ", _.divide(7,2));
 
-// const sum = _.add(7, 7);
-// console.log("sum", sum);
+const sum = _.add(7, 7);
+console.log("sum = _.add(7, 7) = ", sum);
 
-const divideArrayToParts = _.chunk(['a', 'b', 'c', 'd'], 5);
-console.log("divideArrayToParts", divideArrayToParts);
+const divideArrayToParts = _.chunk(['a', 'b', 'c', 'd'], 3);
+console.log("_.chunk(['a', 'b', 'c', 'd'], 3)", divideArrayToParts);
+console.log("divideArrayToParts[0]", divideArrayToParts[0]);
+console.log("...divideArrayToParts", ...divideArrayToParts);
+
+console.log('--------------------------');
+// const stamp = "dd.mm.yyyy";
+// _.defer(function(stamp) {
+//   console.log(_.now() - stamp);
+// }, _.now());
+
+const years = (_.now() / 1000 / 60 / 60 / 24 / 365);
+const roundedYears = Math.floor(years);
+console.log("years", years);
+const months = ((_.now() / 1000 / 60 / 60 / 24 / 365) - roundedYears) * 365 / 30;
+const roundedMonths = Math.floor(months);
+console.log("months", months);
+const days = (years - roundedYears) * 365;
+console.log("days", days);
+
+const hours = Math.floor(_.now() / 1000 / 60 / 60 );
+const minutes = Math.floor(_.now() / 1000 / 60 );
+const seconds = (_.now() / 1000 / 60 / 60 / 24 / 365);
+
+console.log(roundedYears + ' years ' + roundedMonths + ' months ' + days + ' days' + ' from 01.01.1970'); // _.now() - Gets the timestamp of the number of milliseconds that have elapsed since the Unix epoch (1 January 1970 00:00:00 UTC).
+
+console.log('');
